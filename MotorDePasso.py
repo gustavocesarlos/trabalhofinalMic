78adcf589891150f450184998ed46ef2d19c6d30
Rodolfo Barcelar <ro_barcelar@yahoo.com.br>
2015-12-02 15:24:39 -0200

    Update MotorDePasso.py

--- MotorDePasso.py
+++ MotorDePasso.py
@@ -1,65 +1,46 @@
-####IMPORTS
-import time, sys, signal, atexit
-import pyupm_uln200xa as upmULN200XA
-
-# Instantiate a Stepper motor on a ULN200XA Darlington Motor Driver
+import time, sys, signal, atexit                                   
+import pyupm_uln200xa as upmULN200XA                                 
+      
+# Instantiate a Stepper motor on a ULN200XA Darlington Motor Driver  
 # This was tested with the Grove Geared Step Motor with Driver
-# Código retirado do site https://software.intel.com/en-us/iot/hardware/sensors/uln200xa-stepper-driver
-# Código modificado do original
+ 
+# Instantiate a ULN2003XA stepper object                             
+myUln200xa = upmULN200XA.ULN200XA(4096, 8, 9, 10, 11)                
 
-	 
+## Exit handlers ##               
+# This stops python from printing a stacktrace when you hit control-C
+def SIGINTHandler(signum, frame):                                    
+        raise SystemExit                                             
+ 
+# This lets you run code on exit,          
+# including functions from myUln200xa                                      
+def exitHandler():                                                         
+        print "Exiting"                                              
+        sys.exit(0)                                                  
 
-	# Inicializando um ULN2003XA stepper objeto
-	# ULN200XA(stepsPerRevolution, i1, i2, i3, i4)
-	# i1 a i4 são os pinos ligados à placa (8 a 11)
-	myUln200xa = upmULN200XA.ULN200XA(4096, 8, 9, 10, 11)	 
+# Register exit handlers                   
+atexit.register(exitHandler)                                               
+signal.signal(signal.SIGINT, SIGINTHandler) 
+###########################################
+#Setando a velocidade e a direcao:                                   
+myUln200xa.setSpeed(5) # 5 RPMs                                      
+myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CW) #sentido horario
 
-	#########################################################
+print "Abrindo o portao..."                                          
+myUln200xa.stepperSteps(300)                                         
 
-	## Exit handlers ##
-	# This stops python from printing a stacktrace
-	# when you hit control-C
-	def SIGINTHandler(signum, frame):
-	raise SystemExit	
-
-	# This lets you run code on exit,
-	# including functions from myUln200xa
-	def exitHandler():
-	    print "Exiting"
-	    sys.exit(0)
-	 
-
-	# Register exit handlers
-
-	atexit.register(exitHandler)
-	signal.signal(signal.SIGINT, SIGINTHandler)	 
-
-	#######################################################	
-
-	##Controle da velocidade do motor
-	myUln200xa.setSpeed(5) # 5 RPMs
-
-	##DIR_CW = sentido antihorário
-	myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CW)	 
-
-	print "Rotating 1 revolution clockwise."
-
-	##Setando os passos do motor
-	myUln200xa.stepperSteps(4096)	 
-
-	print "Sleeping for 2 seconds..."
-	time.sleep(2)
-
-	 
-
-	print "Rotating 1/2 revolution counter clockwise."
-
-	##DIR_CCW = Sentido horário
-	myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CCW)
-	##Passos
-	myUln200xa.stepperSteps(2048)	 
-
-	# release
-	myUln200xa.release()	 
-
-	# exitHandler is called automatically
+print "Sleeping for 3 seconds..."                                    
+time.sleep(3)                                                        
+###########################################                          
+#Setando a velocidade e a direcao:                                   
+myUln200xa.setSpeed(5) # 5 RPMs                                      
+myUln200xa.setDirection(upmULN200XA.ULN200XA.DIR_CCW) #sentido anti-horario
+ 
+print "Fechando o portao..."                                               
+myUln200xa.stepperSteps(300)                                               
+time.sleep(1)                                                              
+########################################### 
+# release                                                            
+myUln200xa.release()                                                 
+ 
+# exitHandler is called automatically 
